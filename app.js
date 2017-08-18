1ff620fa022557cb7df2913527bf9f409294db1f
Sandra <sandraultreras@gmail.com>
2017-08-17 18:49:10 -0700

    Getting total Cookies sold

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
+var branches = [];
+
+// function branches(minCust, maxCust, avgSale) {
+//   this.minCust = minCust;
+//   this.maxCust = maxCust;
+//   this.avgSale = avgSale;
+//   this.hoursStore = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"],
+// };
+
 var pioneer = {
-  mincust:17,
-  maxcust:88,
-  avgsale:5.2,
-}
+  name: "Pioneer Square",
+  minCust:17,
+  maxCust:88,
+  avgSale:5.2,
+  storeHours: ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"],
+  storeCookies: [],
+  totalCookiesSold: 0,
+//Generate a random number of customers per hour using math.random
+  randomCustomersHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+},
+//Calculate number of cookies sold per hour using avgSale and randomCustomersHour
+  hourlySales: function() {
+    return (this.randomCustomersHour() * this.avgSale);
+},
+  cookiesSoldHour: function() {
+    for (var i = 0; i < this.hoursStore.length; i++) {
+      this.storeCookies.push(this.hourlySales());
+    }
+  },
+
+  displayCookies: function() {
+    var display = document.getElementById("display-pioneer");
+    display.innerHTML = "<h3>" + this.name + "</h3>"
+    display.innerHTML += "<ul>"
+    for (var i = 0; i < this.storeHours.length; i++) {
+      display.innerHTML += "<li>" + this.storeHours[i] + ": " + this.storeCookies[i] + "</li>"
+    }
+    display.innerHTML += "<ul>"
+  },
+  
+  totalCookiesSold: function () {
+    var Total = 0;
+    for (var i = 0; i < this.storeCookies.length; i++) {
+      Total += this.storeCookies[i];
+    }
+    return Total;
+  },
+
+};
+
 
 var airport = {
-  mincust:6,
-  maxcust:24,
-  avgsale:1.2,
+    name: "Pioneer Square",
+    minCust:17,
+    maxCust:88,
+    avgSale:5.2,
+    hoursStore: ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"],
+    storeCookies: [],
+    totalCookiesSold: 0,
 }
 
 var washington = {
@@ -28,9 +78,12 @@ var pearl = {
   avgsale:2.6,
 }
 
-function createTable() {
-  for (var index = 0; index < pioneer.length; index ++){
-    var studentRow = getStudentInfo(students[index]);
-    document.getElementById('cookies').innerHTML += studentRow;
-  }
-}
+
+// function getBranchInfo(branchObject) {
+//   var rowData = "<tr><td>" + branchObject.mincust + "</td>";
+//   rowData += "<td>" + branchObject.maxcust + "</td>";
+//   rowData += "<td>" + branchObject.avgsale + "</td></tr>";
+//   return rowData;
+// }
+
+pioneer.displayCookies();
